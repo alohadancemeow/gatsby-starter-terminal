23c2242ccce1b084a8e7a4e44fabf753cdb17d47
Alohadancemeow <nawongsak@gmail.com>
2021-07-27 13:07:53 +0700

    terminal blog template

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,69 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    name: "gatsby-starter-terminal",
-    description: "A Gatsby starter for @pauliescanlon/gatsby-theme-terminal",
-    keywords: ["tech", "blog", "boop"],
-    siteUrl: "https://gatsby-starter-terminal.netlify.com",
-    siteImage: "terminal-open-graph-image.jpg",
+    name: 'Paul Scanlon clone',
+    description:
+      'Jamstack Developer: React, Gatsby, JavaScript, TypeScript, CSS-in-Js, Storybook, TDD & a tiny bit of Apollo/GraphQL/Node',
+    keywords: [
+      'React',
+      'Gatsby',
+      'JavaScript',
+      'TypeScript',
+      'Flow',
+      'styled-components',
+      'Theme UI',
+      'Jest',
+      'Enzyme',
+      'React Testing Libary',
+      'Node.js',
+      'Fauna',
+      'Jamstack',
+      'Component Library',
+      'Serverless Functions',
+    ],
+    siteUrl: 'https://paulie.dev',
+    siteImage: 'https://paulie.dev/images/paulie-open-graph-image.jpg',
     profileImage: ``,
     lang: `en`,
     config: {
-      sidebarWidth: 280
-    }
+      sidebarWidth: 200,
+    },
   },
   plugins: [
+    `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-mdx-embed`,
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
     {
-      resolve: "@pauliescanlon/gatsby-theme-terminal",
+      resolve: `gatsby-plugin-sharp`,
       options: {
-        source: ["posts"]
-      }
-    }
-  ]
-};
+        defaults: {
+          quality: 70,
+          formats: ['auto', 'webp', 'avif'],
+          placeholder: 'blurred',
+        },
+      },
+    },
+    {
+      resolve: '@pauliescanlon/gatsby-theme-terminal',
+      options: {
+        source: ['posts'],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-76055934-4',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        exclude: [`/dummy-page/`],
+      },
+    },
+  ],
+}
--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,29 +1,109 @@
-import codeTheme from "@theme-ui/prism/presets/night-owl-light.json";
-import baseTheme from "@pauliescanlon/gatsby-theme-terminal/src/gatsby-plugin-theme-ui";
+import codeTheme from '@theme-ui/prism/presets/shades-of-purple.json'
+import baseTheme from '@pauliescanlon/gatsby-theme-terminal/src/gatsby-plugin-theme-ui'
 
 export default {
   ...baseTheme,
   colors: {
     ...baseTheme.colors,
-    text: "#222222",
-    muted: "#666666",
-    primary: "#FF4081",
-    secondary: "#03A9F4",
-    success: "#FFEB3B",
-    background: "#ffffff",
-    surface: "#f3f3f3"
+    text: '#FFFFFF',
+    muted: '#8b87ea',
+    primary: '#f056c7',
+    secondary: '#c39eff',
+    error: '#ff4343',
+    success: '#58e6d9',
+    background: '#131127',
+    surface: '#232140',
+    highlight: '#ffeb3b',
+    placeholder: '#4e4b85',
   },
+
   styles: {
     ...baseTheme.styles,
+    root: {
+      ...baseTheme.styles.root,
+      '.mdx-embed': {
+        '.twitter-tweet-mdx-embed': {
+          minHeight: 500,
+        },
+      },
+    },
     p: {
+      ...baseTheme.styles.p,
+      a: {
+        ...baseTheme.styles.p.a,
+        color: 'secondary',
+        wordBreak: 'break-word',
+      },
       code: {
         ...baseTheme.styles.p.code,
-        color: "inherit"
-      }
+        color: 'inherit',
+        fontSize: '14px',
+        wordBreak: 'break-word',
+        backgroundColor: 'surface',
+      },
+      mark: {
+        color: 'background',
+        backgroundColor: 'highlight',
+      },
     },
     pre: {
       ...baseTheme.styles.pre,
-      ...codeTheme
-    }
-  }
-};
+      ...codeTheme,
+    },
+    a: {
+      ...baseTheme.styles.a,
+      color: 'secondary',
+      button: {
+        cursor: 'pointer',
+      },
+    },
+  },
+
+  buttons: {
+    ...baseTheme.buttons,
+    success: {
+      ...baseTheme.buttons.success,
+    },
+    primary: {
+      ...baseTheme.buttons.primary,
+      ':disabled': {
+        cursor: 'not-allowed',
+        backgroundColor: 'surface',
+      },
+    },
+    ghost: {
+      ...baseTheme.buttons.primary,
+      color: 'muted',
+      backgroundColor: 'background',
+      transition: '.2s linear background-color',
+      ':hover': {
+        backgroundColor: 'surface',
+      },
+    },
+  },
+
+  forms: {
+    ...baseTheme.forms,
+    label: {
+      ...baseTheme.forms.label,
+      color: 'primary',
+    },
+    input: {
+      ...baseTheme.forms.input,
+      '::placeholder': {
+        color: 'placeholder',
+      },
+    },
+  },
+
+  cards: {
+    primary: {
+      ...baseTheme.cards.primary,
+      transition: '.2s linear box-shadow, .2s ease-in-out transform',
+      ':hover': {
+        transform: 'translateY(-0.25rem)',
+        boxShadow: 2,
+      },
+    },
+  },
+}
